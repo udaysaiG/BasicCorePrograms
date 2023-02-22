fcd4d19bd29f5681b057a545ef605b048c61ac14
udaysaigalla <udaysaigalla8@gmail.com>
2023-02-22 15:54:48 +0530

    BasicCorePrograms

--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -1,35 +1,33 @@
-﻿namespace BasicCorePrograms
+﻿using System;
+
+class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        if (args.Length == 0)
         {
-            if (args.Length == 0)
+            Console.WriteLine("Please provide a number as a command-line argument.");
+            return;
+        }
+
+        int n = int.Parse(args[0]);
+
+        Console.WriteLine("Prime factors of " + n + ":");
+        for (int i = 2; i * i <= n; i++)
+        {
+            while (n % i == 0)
             {
-                Console.WriteLine("Please provide a value for N as a command-line argument.");
-                return;
+                Console.WriteLine(i);
+                n /= i;
             }
+        }
 
-            int n = int.Parse(args[0]);
-
-            if (n == 0)
-            {
-                Console.WriteLine("N should not be 0.");
-                return;
-            }
-
-            double harmonicNumber = 0;
-
-            for (int i = 1; i <= n; i++)
-            {
-                harmonicNumber += 1.0 / i;
-            }
-
-            Console.WriteLine("The " + n + "th harmonic number is: " + harmonicNumber);
+        if (n > 1)
+        {
+            Console.WriteLine(n);
         }
     }
-        }
-    
+}
 
-        
-    
+
+
