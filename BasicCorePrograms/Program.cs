9210879c3b72b23666128c9148260995b5a01ee5
udaysaigalla <udaysaigalla8@gmail.com>
2023-02-22 15:36:52 +0530

    BasicCorePrograms

--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -4,24 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter a year (4 digits):");
-            string input = Console.ReadLine();
-            int year;
-
-            while (!int.TryParse(input, out year) || input.Length != 4)
+            if (args.Length == 0)
             {
-                Console.WriteLine("Invalid input. Please enter a 4-digit year:");
-                input = Console.ReadLine();
+                Console.WriteLine("Please provide a value for N as a command-line argument.");
+                return;
             }
 
-            if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
+            int n = int.Parse(args[0]);
+
+            if (n < 0 || n >= 31)
             {
-                Console.WriteLine("{0} is a leap year.", year);
+                Console.WriteLine("N should be between 0 and 30 inclusive, since 2^31 overflows an int.");
+                return;
             }
-            else
+
+            int powerOfTwo = 1;
+
+            Console.WriteLine("Powers of 2 less than or equal to 2^" + n + ":");
+            Console.WriteLine(powerOfTwo);
+
+            for (int i = 1; i <= 30; i++)
             {
-                Console.WriteLine("{0} is not a leap year.", year);
+                powerOfTwo *= 2;
+                Console.WriteLine(powerOfTwo);
             }
         }
     }
 }
+        
+    
